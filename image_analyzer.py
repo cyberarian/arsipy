3f53e82fc24f7376477ba7d384058c5a88a5b611
Adnuri Mohamidi <cyberariani@gmail.com>
2025-02-21 14:38:52 +0700

    Update image_analyzer.py

--- image_analyzer.py
+++ image_analyzer.py
@@ -16,8 +16,8 @@ import re
 from typing import Dict, List, Tuple
 
 # Configure Tesseract path
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' # windows
-#pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract' # linux
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' # windows
+pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract' # linux
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
